71704a7a4909be26a5098d3ca3164cb258471208
Vishnu <vishnuprasadkorada@gmail.com>
2023-06-09 18:39:53 +0530

    cleaned code

---
+++ README.md
@@ -0,0 +1,5 @@
+# Audio-Player
+
+An Audio Player to play your local Music files.🎺🎵🎶 
+
+https://vishnuprasad2004.github.io/Audio-Player/
--- script.js
+++ script.js
@@ -1,48 +1,86 @@
+/*
+    > Make it read any music file from songs
+
+ */
+
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
 const audioPlayer = document.getElementById("player");
 const audioName = document.getElementById("audioName");
 const playlist = document.getElementsByClassName("songName"); 
-
-let audioNames = ["Havana - Camila Cabello",      
-                "Blinding Lights - The Weeknd",  
-                "Heat Waves - Glass Animals ",  
-                "Unstoppable - Sia", 
-                "Snowman - Sia", 
-                "Beggin' - Maneskin", 
-                "Enemy - Imagine Dragons", 
-                "Infinity - Jaymes Young", 
-                "STAY - KID LAROI (feat. Justin Bieber)",  
-                "Stunnin' - Curtis Waters",
-                "Flowers - Miley Cyrus",
-                "Faded - Alan Walker",
-                "Senorita - Shawn Mendes Camila Cabello",
-                "As it Was - Harry Styles"];    
-
-
-let audioFiles = ["Havana.mp3",
-                "Blinding lights.mp3",
-                "Heat waves.mp3",
-                "Sia - Unstoppable.mp3",
-                "snowman.mp3",
-                "Beggin'.mp3",
-                "Enemy (with JID) - from the series Arcane League of Legends.mp3",//6
-                "Infinity.mp3",
-                "STAY (with Justin Bieber).mp3",
-                "Stunnin' (feat. Harm Franklin).mp3", 
-                "Miley Cyrus - Flowers.mp3",
-                "Faded.mp3",
-                "Shawn Mendes   Camila Cabello - Senorita   1.mp3",
-                "As It Was.mp3"];
-
-
+const playlistContainer = document.getElementById('songs-container');
 let idx = 0;
 
-audioName.textContent = audioNames[0];
-audioPlayer.src = audioFiles[0];
+class Song {
+    constructor(songName, location) {
+        this.songName = songName;
+        this.location = location;
+    }
+}
+
+const songs = [
+            new Song("Havana - Camila Cabello","songs\\Havana.mp3"),
+            new Song("Blinding Lights - The Weeknd", "songs\\Blinding lights.mp3"),
+            new Song("Heat Waves - Glass Animals", "songs\\Heat waves.mp3"),
+            new Song("Unstoppable - Sia", "songs\\Sia - Unstoppable.mp3"),
+            new Song("Snowman - Sia", "songs\\snowman.mp3"),
+            new Song("Beggin' - Maneskin", "songs\\Beggin'.mp3"),
+            new Song("Enemy - Imagine Dragons", "songs\\Enemy (with JID) - from the series Arcane League of Legends.mp3"),
+            new Song("Infinity - Jaymes Young", "songs\\Infinity.mp3"),
+            new Song("STAY - KID LAROI (feat. Justin Bieber)", "songs\\STAY (with Justin Bieber).mp3"),
+            new Song("Stunnin' - Curtis Waters", "songs\\Stunnin' (feat. Harm Franklin).mp3"),
+            new Song("Flowers - Miley Cyrus", "songs\\Miley Cyrus - Flowers.mp3"),
+            new Song("Faded - Alan Walker", "songs\\Faded.mp3"),
+            new Song("Senorita - Shawn Mendes Camila Cabello", "songs\\Shawn Mendes   Camila Cabello - Senorita   1.mp3"),
+            new Song("As it Was - Harry Styles","songs\\As It Was.mp3"),
+            new Song("Good 4 u - Olivia Rodrigo", "songs\\good 4 u.mp3")
+            ];
+
+
+for(let i=0; i<songs.length; i++) {
+    let song = document.createElement('div');
+    song.textContent = songs[i].songName;
+    song.classList = 'songName';
+    playlistContainer.appendChild(song);
+}
+
+for (let i = 0; i < playlist.length; i++) {
+    playlist[i].onclick = function() {
+        
+        audioName.textContent = songs[i].songName;
+        audioPlayer.src = songs[i].location;
+        playlist[i].style.backgroundColor = "#ffffff";
+
+        idx = i;
+
+        for(let j = 0; j<i; j++) {
+            playlist[j].style.backgroundColor = "#ffffffb6";
+        }
+
+        for(let j = i+1; j<playlist.length; j++) {
+            playlist[j].style.backgroundColor = "#ffffffb6";
+        }
+
+    }
+    
+}
+
+audioName.textContent = songs[0].songName;
+audioPlayer.src = songs[0].location;
 playlist[idx].style.backgroundColor = "#ffffff";
 
-window.addEventListener("keydown",util);
+
+window.addEventListener("keydown",({ key }) => {
+    switch(key) {
+        case "ArrowRight":
+            next();
+            break;
+
+        case "ArrowLeft":
+            previous();
+            break;
+    }
+});
 
 //previous button
 prevBtn.addEventListener("click",previous);
@@ -50,64 +88,61 @@ prevBtn.addEventListener("click",previous);
 //next button
 nextBtn.addEventListener("click",next);
 
-if(audioPlayer.ended == true ) {
-    next();
-}
+function isEnded() {
 
-function util(event) {
-    console.log(event.key);
-    switch(event.key) {
-        // case " ":
-        //     if(audioPlayer.paused == false) {
-        //         audioPlayer.paused = true;
-        //     }else {
-        //         audioPlayer.paused = false;
-        //     }
-        //     break;
-
-        case "ArrowDown":
-            next();
-            break;
-
-        case "ArrowUp":
-            previous();
-            break;
-
-        default:
-            break;
+    if(audioPlayer.ended() == true ) {
+        next();
     }
 }
 
+// function util(event) {
+//     switch(event.key) {
+//         // case " ":
+//         //     if(audioPlayer.paused == false) {
+//         //         audioPlayer.paused = true;
+//         //     }else {
+//         //         audioPlayer.paused = false;
+//         //     }
+//         //     break;
+
+//         case "ArrowRight":
+//             Song.next();
+//             break;
+
+//         case "ArrowLeft":
+//             Song.previous();
+//             break;
+
+//         default:
+//             break;
+//     }
+// }
 
 function previous() {
     idx = idx - 1;
     if(idx < 0) {
-        idx = audioFiles.length-1;
+        idx = songs.length-1;
         playlist[0].style.backgroundColor = "#ffffffb6";    
     }
-    audioName.textContent = audioNames[idx];
-    audioPlayer.src = audioFiles[idx];
+    audioName.textContent = songs[idx].songName;
+    audioPlayer.src = songs[idx].location;
     playlist[idx].style.backgroundColor = "#ffffff";
-    if(idx+1 != audioFiles.length) {
+    if(idx+1 != songs.length) {
         playlist[idx+1].style.backgroundColor = "#ffffffb6";
     }
-    
-    
 }
-
 
 function next() {
     idx = idx + 1;
-    if(idx >  audioFiles.length-1) {
+    if(idx >  songs.length-1) {
         idx = 0;
-        playlist[audioFiles.length-1].style.backgroundColor = "#ffffffb6";    
+        playlist[songs.length-1].style.backgroundColor = "#ffffffb6";    
     }
-    audioName.textContent = audioNames[idx];
-    audioPlayer.src = audioFiles[idx];
+    audioName.textContent = songs[idx].songName;
+    audioPlayer.src = songs[idx].location;
     playlist[idx].style.backgroundColor = "#ffffff";
     if(idx-1 != -1) {
         playlist[idx-1].style.backgroundColor = "#ffffffb6";
     }
-    
     
 }
